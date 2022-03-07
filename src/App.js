1fa3b3429eae62d8a584781e88c214967e8c9336
pardomanuel <pardomanuel@hotmail.com>
2022-03-07 01:41:38 -0300

    Cotizador de criptomonedas con react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
+import imagen from './cryptomonedas.png';
+import Formulario from './Component/Formulario';
+import Cotizacion  from './Component/Cotizacion';
+import Spinner from './Component/Spinner';
+import axios from 'axios';
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Imagen = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+
+const Heading = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: #FFF;
+  text-align: left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+  }
+`;
 
 function App() {
+  const [ moneda, guardarMoneda ] = useState('');
+  const [ criptomoneda, guardarCriptomoneda ] = useState('');
+  const [ resultado, guardarResultado ] = useState({});
+  const [ cargando, guardarCargando ] = useState(false);
+
+  useEffect(() => {
+    if(moneda === '') return;
+
+    const cotizarCriptomoneda = async () => {
+      //consultar api para obtener cotizacion
+      const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+      const resultado = await axios.get(url);
+      
+      guardarCargando(true);
+
+      setTimeout(() => {
+        guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda]);
+        guardarCargando(false);
+      }, 1500);
+    }
+
+    cotizarCriptomoneda();
+  }, [moneda, criptomoneda]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <div>
+        <Imagen 
+          src={imagen}
+          alt="imagen cripto"
+        />
+      </div>
+      <div>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+        <Formulario 
+          guardarMoneda = {guardarMoneda}
+          guardarCriptomoneda = {guardarCriptomoneda}
+        />
+        { cargando ? 
+          <Spinner /> 
+        : 
+          <Cotizacion 
+            resultado = {resultado}
+          />
+        }
+      </div>
+    </Contenedor>
   );
 }
 
